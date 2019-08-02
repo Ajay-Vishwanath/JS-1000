be1743a084fd628653cfa2b36c59bfb41cfa1a92
Ajay <ajayvishwanath.94@gmail.com>
2019-08-01 18:01:19 -0700

    Work on LFO Component

--- app/assets/javascripts/synth/lfos.js
+++ app/assets/javascripts/synth/lfos.js
@@ -1,7 +1,48 @@
 class LFO {
     constructor(options){
         this.context = options.context,
-        
+        this.osc = options.createOscillator();
+        this.osc.type = "sine";
+
+        this.depth = options.context.createGain();
+        this.osc.connect(this.depth)
+        this.lfoDepthControl = document.getElementById('lfo-depth')
+        this.depth.value = this.lfoDepthControl.value
+        this.lfoDepthControl.addEventListener('change', this.setLFODepth.bind(this), false);
+
+        this.lfoFrequencyControl = document.getElementById('lfo-frequency')
+        this.osc.frequency.value = this.lfoDepthControl.value
+        this.lfoFrequencyControl.addEventListener('change', this.setLFOFrequency.bind(this), false);
+
+        this.source = null 
+    }
+
+    setLFODepth(){
+        this.depth.value = parseFloat(event.target.value)
+    }
+
+    setLFOFrequency() {
+        this.osc.frequency.value = parseFloat(event.target.value)
+    }
+
+    setParams(param, source){
+        if (source === this.source) {
+            this.source = null 
+            param.disconnect()
+        }
+        else if (source === "amp"){
+
+            this.source = "amp"
+            this.depth.connect(param)
+
+        } else if (source === "filter"){
+            this.source = "filter"
+            this.depth.connect(param)
+
+        } else if (source === "frequency"){
+            this.source = "frequency"
+            this.depth.connect(param)
+        }
     }
 }
 
